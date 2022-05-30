3fd4df9fbeec15106e23aaa2118fac583742edce
DevWeb13 <giuliano.loic@sfr.fr>
2022-05-30 14:48:39 +0200

    feat: (SignUp.jsx): <form className="formGroup">...</form>
    (eslintrc.json): "jsx-a11y/label-has-associated-control": "off",         "jsx-a11y/label-has-for": "off"

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -7,6 +7,44 @@ function SignUp() {
     <div className="container">
       <Header title="HRnet" />
       <Link to="/employees-list">View Current Employees</Link>
+      <h2>Create Employee</h2>
+      <form className="formGroup">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" className="formControl" id="firstName" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" className="formControl" id="lastName" />
+
+        <label htmlFor="dateOfBirth">Date of Birth</label>
+        <input type="date" className="formControl" id="dateOfBirth" />
+
+        <label htmlFor="startDate">Start Date</label>
+        <input type="date" className="formControl" id="startDate" />
+
+        <label htmlFor="street">Street</label>
+        <input type="text" className="formControl" id="street" />
+
+        <label htmlFor="city">City</label>
+        <input type="text" className="formControl" id="city" />
+
+        <label htmlFor="state">State</label>
+        <input type="text" className="formControl" id="state" />
+
+        <label htmlFor="zipCode">Zip Code</label>
+        <input type="text" className="formControl" id="zipCode" />
+
+        <label htmlFor="Department">Department</label>
+        <select className="formControl" id="Department">
+          <option>Sales</option>
+          <option>Marketing</option>
+          <option>Engineering</option>
+          <option>Human Resources</option>
+          <option>Legal</option>
+        </select>
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
